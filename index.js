49ec660b96af2edb0f03bef5ef2029c8d0ebc267
Thomas Rasmussen <info@tomramsussen.me>
2017-08-23 10:16:33 -0400

    Fixed background color issues

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-exports.decorateConfig = (config) => {
-	return Object.assign({}, config, {
-		foregroundColor: '#ebdbb2',
-		backgroundColor: '#282828',
-		borderColor: '#282828',
-		cursorColor: 'rgba(184,187,38, 0.6)',
-		colors: [
-			'#928374',
-			'#fb4934',
-			'#b8bb26',
-			'#fabd2f',
-			'#83a598',
-			'#d3869b',
-			'#8ec97c',
-			'#fbf1c7',
-			'#ebdbb2',
-			'#cc241d',
-			'#98971a',
-			'#d79921',
-			'#458588',
-			'#b16286',
-			'#689d6a',
-			'#a89984'
-		],
-  })
-}
+exports.decorateConfig = config => {
+  return Object.assign({}, config, {
+    foregroundColor: '#ebdbb2',
+    backgroundColor: '#262626',
+    borderColor: '#262626',
+    cursorColor: 'rgba(184,187,38, 0.6)',
+    colors: [
+      '#928374',
+      '#fb4934',
+      '#b8bb26',
+      '#fabd2f',
+      '#83a598',
+      '#d3869b',
+      '#8ec97c',
+      '#fbf1c7',
+      '#ebdbb2',
+      '#cc241d',
+      '#98971a',
+      '#d79921',
+      '#458588',
+      '#b16286',
+      '#689d6a',
+      '#a89984',
+    ],
+  });
+};
